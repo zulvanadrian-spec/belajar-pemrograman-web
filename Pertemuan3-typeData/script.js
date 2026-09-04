0eddd39c605695ac2c10c9d58ddceb497420e7c9
zulvan <zulvanadrian@gmail.com>
2026-09-04 09:36:15 +0700

    Pertemuan3-type data

---
+++ Pertemuan3-typeData/script.js
@@ -0,0 +1,31 @@
+// ----- type data ------
+
+// cek type data string
+let dataStr = "amba";
+console.log("data =",dataStr)
+console.log("type data ->",typeof dataStr);
+
+// cek type data number
+let dataNumber = 13.323;
+console.log("data =",dataNumber);
+console.log("type data ->", typeof dataNumber)
+
+// cek type data boolean
+let dataBoolean = false;
+console.log("data =",dataBoolean);
+console.log("type data ->",typeof dataBoolean);
+
+// cek type data undifinded
+let dataKosong;
+console.log("data =",dataKosong);
+console.log("type data ->",typeof dataKosong)
+
+// ubah type data str -> number
+dataStr = 2;
+console.log("data =",dataStr)
+console.log("type data ->",typeof dataStr);
+
+// ubah / isi data undifined whatever
+dataKosong = 2;
+console.log("data =",dataKosong);
+console.log("data undifined jadi ->",typeof dataKosong)
