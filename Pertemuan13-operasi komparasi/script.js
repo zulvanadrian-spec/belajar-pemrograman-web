a1ea7da109f3434b28c9b26e4994492f32779dbc
zulvan <zulvanadrian@gmail.com>
2026-09-15 17:34:48 +0700

    operasi komparasi

---
+++ Pertemuan13-operasi komparasi/script.js
@@ -0,0 +1,31 @@
+// == (equality)
+console.log(`5 == "5" : ${5 == "5"}`)
+console.log(`5 == 5 : ${5 == 5}`)
+
+// != (inequality)
+console.log(`5 != 5 : ${5 != 5}`)
+console.log(`5 != "5" : ${5 != "5"}`)
+
+// === (strict equality)
+console.log(`5 === 5 : ${5 === 5}`)
+console.log(`5 === "5" : ${5 === "5"}`)
+
+// !== (strict inequality)
+console.log(`5 !== 5 : ${5 !== 5}`)
+console.log(`5 !== "5" : ${5 !== "5"}`)
+
+// >
+console.log(`10 > 7 : ${10 > 7}`)
+console.log(`10 > 10 : ${10 > 10}`)
+
+// >=
+console.log(`10 >= 7 : ${10 >= 7}`)
+console.log(`10 >= 10 : ${10 >= 10}`)
+
+// <
+console.log(`10 < 7 : ${10 < 7}`)
+console.log(`10 < 10 : ${10 < 10}`)
+
+// <=
+console.log(`10 <= 7 : ${10 <= 7}`)
+console.log(`10 <= 10 : ${10 <= 10}`)
