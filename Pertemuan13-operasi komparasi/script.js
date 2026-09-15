62b34058df7c75b405e4f9dce89d5d496c913fbd
zulvan <zulvanadrian@gmail.com>
2026-09-15 17:41:55 +0700

    operasi komparasi

--- Pertemuan13-operasi komparasi/script.js
+++ Pertemuan13-operasi komparasi/script.js
@@ -1,31 +1,39 @@
 // == (equality)
+console.log("--- == (EQUALITY) ---")
 console.log(`5 == "5" : ${5 == "5"}`)
 console.log(`5 == 5 : ${5 == 5}`)
 
 // != (inequality)
+console.log("--- != (INEQUALITY) ---")
 console.log(`5 != 5 : ${5 != 5}`)
 console.log(`5 != "5" : ${5 != "5"}`)
 
 // === (strict equality)
+console.log("--- === (STRICT EQUALITY) ---")
 console.log(`5 === 5 : ${5 === 5}`)
 console.log(`5 === "5" : ${5 === "5"}`)
 
 // !== (strict inequality)
+console.log("--- !== (STRICT INEQUALITY) ---")
 console.log(`5 !== 5 : ${5 !== 5}`)
 console.log(`5 !== "5" : ${5 !== "5"}`)
 
 // >
+console.log("--- > (GREATER THAN) ---")
 console.log(`10 > 7 : ${10 > 7}`)
 console.log(`10 > 10 : ${10 > 10}`)
 
 // >=
+console.log("--- >= (GREATHER THAN OR EQUAL TO) ---")
 console.log(`10 >= 7 : ${10 >= 7}`)
 console.log(`10 >= 10 : ${10 >= 10}`)
 
 // <
+console.log("--- < (LESS THAN) ---")
 console.log(`10 < 7 : ${10 < 7}`)
 console.log(`10 < 10 : ${10 < 10}`)
 
 // <=
+console.log("--- <= (LESS THAN OR EQUQL TO) ---")
 console.log(`10 <= 7 : ${10 <= 7}`)
 console.log(`10 <= 10 : ${10 <= 10}`)
