0640bb10d156b4c7748cc26dd39aaae07928dc70
zulvan <zulvanadrian@gmail.com>
2026-09-18 14:29:27 +0700

    IF statement

---
+++ Pertemuan15-IF statement/script.js
@@ -0,0 +1,12 @@
+let name = "gatot";
+
+console.log("nama : ",  name)
+
+if(name === "amba"){
+    console.log("hi amba");
+}
+
+else{
+    console.log("omakk bukan mas ambaa")
+}
+console.log("udahan")
